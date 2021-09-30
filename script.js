ddc384c2f28ea9b2f3a563dd8d50f337b48a3cb2
Amara Stephens <amarastephens@Amaras-MacBook-Pro.local>
2021-09-29 20:28:27 -0500

    submit

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-var friends = ["Ryan", "Autumn", "Zac", "Kathryn", "Ella"] 
-var locations = ["Beach", "Forest", "House", "City Hall", "Hawaii", "Mexico", "Bryant Denny", "Jordan Hare", "Carrigan's", "Innisfree"]
-var weapons = ["knife", "gun", "apple", "stick", "dog", "wire", "bat", "glass", "can", "banana", "pear", "hand", "sword", "baseball", "rope", "pie", "mango", "mase", "watermelon", "kiwi" ]
+var friends = ["Ryan", "Autumn", "Zac", "Kathryn", "Ella"]
+var locations = ["Beach", "Forest", "House", "City Hall", "Hawaii", "Moe's", "Redmont", "The Grocery", "Carrigan's", "Innisfree"]
+var weapons = ["knife", "gun", "axe", "stick", "rabid dog", "wire", "bat", "glass", "can", "banana", "pear", "hand", "sword", "baseball", "rope", "pole", "mango", "mase", "watermelon", "kiwi"]
 
 
 
-for (var i=1; i<=100; i++) {
+for (var i = 1; i <= 100; i++) {
 
-var h3 = document.createElement("h3"); 
-document.body.appendChild(h3); 
-h3.textContent = ("Accusation " + i)
-h3.addEventListener("click", getAlert(i)) 
+    var h3 = document.createElement("h3");
+    document.body.appendChild(h3);
+    h3.textContent = ("Accusation " + i)
+    h3.addEventListener("click", getAlert)
 }
 
-function getAlert (i) {
+function getAlert() {
     var friendsArr = friends[i % friends.length]
     var locationsArr = locations[i % locations.length]
     var weaponsArr = weapons[i % weapons.length]
-function clickFunc () {
-     alert("I accuse " + friendsArr + " with the " + weaponsArr + " in the " + locationsArr + "!")
+    function clickFunc() {
+        alert("I accuse " + friendsArr + " with the " + weaponsArr + " in the " + locationsArr + "!")
 
-}
-return clickFunc
+    }
+    return clickFunc
 }
